9fface80b3eb269e6fa42d2ae5db0b693acd66a3
rafiksalhi <72387211+rafiksalhi@users.noreply.github.com>
2020-10-08 19:19:52 +0100

    Update mountmydisk.ps1

--- mountmydisk.ps1
+++ mountmydisk.ps1
@@ -1,1 +1,1 @@
-install-windowsfeature -name web-server
+Get-Disk | Where partitionstyle -eq ‘raw’ | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel “disk2” -Confirm:$false
